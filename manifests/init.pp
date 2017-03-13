8bd126e87f101da2d47d07d0faf94a706e065a39
Peter Mumenthaler <pmumenthaler@gmail.com>
2017-03-13 22:02:47 +0100

    add jail config

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,4 +5,12 @@ class fail2ban {
   service { 'fail2ban':
     ensure => 'running';
   }
+  file { '/etc/fail2ban/jail.conf':
+    mode   => '0644',
+    source => [
+      "puppet:///modules/nfs/conf.${host}",
+      "puppet:///modules/nfs/conf.${operatingsystem}",
+      'puppet:///modules/fail2ban/jail.conf'
+    ]
+  }
 }
