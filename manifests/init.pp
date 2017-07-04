d318729bbc58e6672b5a39376599d45e8420a4b4
Peter Mumenthaler <pmumenthaler@gmail.com>
2017-07-04 14:17:46 +0200

    set firewallcmd-allports for bannaction

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,13 +2,35 @@ class fail2ban {
   package { 'fail2ban':
     ensure => 'latest';
   }
+  package { 'fail2ban-systemd':
+    ensure => 'latest';
+  }
+
+  package { 'fail2ban-firewalld':
+    ensure => 'latest';
+  }
+
+  package { 'fail2ban-server':
+    ensure => 'latest';
+  }
+
   service { 'fail2ban':
     ensure => 'running';
   }
+
   service { 'firewalld':
     ensure => 'running';
   }
+
   $services = [fail2ban,firewalld]
+
+  file { '/etc/fail2ban/jail.d/00-firewalld.conf':
+    mode   => '0644',
+    source => [
+      'puppet:///modules/fail2ban/00-firewalld.conf'
+    ]
+  } -> Service[$services] 
+
   file { '/etc/fail2ban/jail.conf':
     mode   => '0644',
     source => [
